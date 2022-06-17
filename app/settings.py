49ec427d83540883a2ef6394d9c25c0196be82e9
George Patterson <gp00482@surrey.ac.uk>
2022-06-17 14:48:17 +0100

    Update db settings

--- app/settings.py
+++ app/settings.py
@@ -150,16 +150,13 @@ AUTHENTICATION_BACKENDS = (
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if os.environ.get("DATABASE_URL"):
-    db = env.db()
-else:
-    db = {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("DB_HOST"),
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASS"),
-    }
+db = {
+    "ENGINE": "django.db.backends.postgresql",
+    "HOST": os.environ.get("DB_HOST"),
+    "NAME": os.environ.get("DB_NAME"),
+    "USER": os.environ.get("DB_USER"),
+    "PASSWORD": os.environ.get("DB_PASS"),
+}
 
 DATABASES = {"default": db}
 
